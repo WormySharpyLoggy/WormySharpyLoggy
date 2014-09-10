5b98d25c5d68b92ad0bcba97797e9a4461f34d7a
Christopher St. John <chrisjstjohn@gmail.com>
2014-09-10 15:06:23 -0400

    Added shape count for #78576058

--- app/src/main/java/fakesetgame/seniordesign/model/Tile.java
+++ app/src/main/java/fakesetgame/seniordesign/model/Tile.java
@@ -9,7 +9,12 @@ public class Tile {
     private Shading shading;
     private Color color;
 
-    public Tile(Shape shape, Shading shading, Color color){
+    public Tile(Shape shape, Shading shading, Color color, int shapeCount){
+
+        if (shapeCount < 1 || shapeCount > 3){
+            throw new IllegalArgumentException("Value of shapeCount must be between 1 and 3.");
+        }
+        
         this.shape = shape;
         this.shading = shading;
         this.color = color;
