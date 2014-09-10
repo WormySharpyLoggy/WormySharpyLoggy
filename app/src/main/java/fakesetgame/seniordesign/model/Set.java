8ed8aa3f2a05ebfb747b4a5d350673c97f698410
Christopher St. John <chrisjstjohn@gmail.com>
2014-09-10 15:11:32 -0400

    Added some more argument checks for #78576058

---
+++ app/src/main/java/fakesetgame/seniordesign/model/Set.java
@@ -0,0 +1,13 @@
+package fakesetgame.seniordesign.model;
+
+/**
+ * Created by Chris on 9/10/2014.
+ */
+public class Set {
+
+    public static boolean isValidSet(Tile tile1, Tile tile2 Tile tile3){
+        if(tile1 == null || tile2 == null || tile3 == null){
+            return false;
+        }
+    }
+}
--- app/src/main/java/fakesetgame/seniordesign/model/Tile.java
+++ app/src/main/java/fakesetgame/seniordesign/model/Tile.java
@@ -11,10 +11,19 @@ public class Tile {
 
     public Tile(Shape shape, Shading shading, Color color, int shapeCount){
 
+        if(shape == null){
+            throw new IllegalArgumentException("Shape value cannot be null.");
+        }
+        if(shading == null){
+            throw new IllegalArgumentException("Shading value cannot be null.");
+        }
+        if(color == null){
+            throw new IllegalArgumentException("Color value cannot be null.");
+        }
         if (shapeCount < 1 || shapeCount > 3){
             throw new IllegalArgumentException("Value of shapeCount must be between 1 and 3.");
         }
-        
+
         this.shape = shape;
         this.shading = shading;
         this.color = color;
