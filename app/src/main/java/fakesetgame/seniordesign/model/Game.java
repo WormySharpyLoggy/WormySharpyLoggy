fc42a2ee5279f074211a53c13d615749073ea047
SamiMuhanna <sami.muhanna92@gmail.com>
2014-10-03 14:27:32 -0400

    Game State
    includes a way to keep track of sets found, and some getter functions

---
+++ app/src/main/java/fakesetgame/seniordesign/model/Game.java
@@ -0,0 +1,41 @@
+package fakesetgame.seniordesign.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Sami on 9/24/2014.
+ */
+public class Game {
+
+    Board board;
+    int timeElapsed;
+    int score;
+    Set<Set> trackSet = new HashSet<Set>();
+    Set<Tile> tileSet = new HashSet<Tile>();
+
+    public boolean attemptSet(Tile tile1, Tile tile2, Tile tile3) {
+        if (trackSet.isValidSet(tile1, tile2, tile3)) {
+            tileSet.add(tile1);
+            tileSet.add(tile2);
+            tileSet.add(tile3);
+            trackSet.add(tileSet);
+            score = score + 1;
+            return True;
+        }
+        return False;
+    }
+    public int getScore() {
+        return score;
+    }
+
+    public int getTimeElapsed() {
+        return timeElapsed;
+    }
+
+
+
+    //the game has a board, timer, score (sets found), keep track of sets found
+
+
+}
