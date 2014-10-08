7af354fda6488f4bc6e44f408032f211bec16b65
Christopher St. John <chrisjstjohn@gmail.com>
2014-10-08 15:31:58 -0400

    Cleaned up UI
    * Set backgrounds to white
    * Made tile layout auto-scaling so everything will fit exactly on screen
    * Changed tile ImageView names to descriptive numbers
    * Made board and tiles variables object variables instead of method
    variables

--- app/src/main/java/fakesetgame/seniordesign/GameScreen.java
+++ app/src/main/java/fakesetgame/seniordesign/GameScreen.java
@@ -5,6 +5,7 @@ import fakesetgame.seniordesign.util.SystemUiHider;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 
 
@@ -16,29 +17,38 @@ import android.widget.ImageView;
  */
 public class GameScreen extends Activity {
 
+    private static final String TAG = "GameScreen";
+    private ImageView[] tiles = new ImageView[9];
+    private Board board = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.game_activity);
 
+        for (int i = 0; i < tiles.length; i++) {
+            tiles[i] = (ImageView) findViewById(getResources().getIdentifier(
+                    "Tile" + Integer.valueOf(i + 1).toString(),
+                    "id",
+                    getPackageName()));
+            tiles[i].setTag(i);
+            if (tiles[i] != null)
+                Log.d(TAG, String.format("tiles[%d] set to Tile%d", i, i + 1));
+            else
+                Log.e(TAG, String.format("Attempted to set tiles[%d] set to Tile%d, but value is null.", i, i + 1));
+        }
+
         BoardSetup();
     }
 
-    private void BoardSetup() {
-        Board b = Board.generateRandom(6);
-        ImageView[] tiles = new ImageView[9];
-        tiles[0] = (ImageView)findViewById(R.id.TileTL);
-        tiles[1] = (ImageView)findViewById(R.id.TileTC);
-        tiles[2] = (ImageView)findViewById(R.id.TileTR);
-        tiles[3] = (ImageView)findViewById(R.id.TileCL);
-        tiles[4] = (ImageView)findViewById(R.id.TileCC);
-        tiles[5] = (ImageView)findViewById(R.id.TileCR);
-        tiles[6] = (ImageView)findViewById(R.id.TileBL);
-        tiles[7] = (ImageView)findViewById(R.id.TileBC);
-        tiles[8] = (ImageView)findViewById(R.id.TileBR);
 
-        for (int i = 0; i < tiles.length; i++)
-            tiles[i].setImageDrawable(b.getTile(i).getDrawable(this));
+    private void BoardSetup() {
+        board = Board.generateRandom(6);
+
+        for (int i = 0; i < tiles.length; i++) {
+            tiles[i].setImageDrawable(board.getTile(i).getDrawable(this));
+            Log.d(TAG, String.format("Set tile image for tiles[%d]", i));
+        }
     }
 }
