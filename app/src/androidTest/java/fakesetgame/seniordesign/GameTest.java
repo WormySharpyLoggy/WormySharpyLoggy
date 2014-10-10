cab5b90d83a1e8e4c936cca94e6b511db14af1fe
Christopher St. John <chrisjstjohn@gmail.com>
2014-10-10 17:48:07 -0400

    Added unit test for game timer

--- app/src/androidTest/java/fakesetgame/seniordesign/GameTest.java
+++ app/src/androidTest/java/fakesetgame/seniordesign/GameTest.java
@@ -7,7 +7,6 @@ import junit.framework.Assert;
 
 import fakesetgame.seniordesign.model.Game;
 import fakesetgame.seniordesign.model.Color;
-import fakesetgame.seniordesign.model.TileSet;
 import fakesetgame.seniordesign.model.Shading;
 import fakesetgame.seniordesign.model.Shape;
 import fakesetgame.seniordesign.model.Tile;
@@ -21,38 +20,64 @@ public class GameTest extends ApplicationTestCase<Application> {
         super(Application.class);
     }
 
-        public void testInvalidAttemptingSet() throws Exception {
-            Game gameState = new Game();
-            int initialScore;
-            boolean isValidFalse;
+    public void testInvalidAttemptingSet() throws Exception {
+        Game gameState = new Game();
+        int initialScore;
+        boolean isValidFalse;
 
-            Tile tile1 = new Tile(Shape.Oval, Shading.Filled, Color.Red, 1);
-            Tile tile2 = new Tile(Shape.Oval, Shading.Filled, Color.Red, 2);
-            Tile tile3 = new Tile(Shape.Oval, Shading.Filled, Color.Green, 1);
+        Tile tile1 = new Tile(Shape.Oval, Shading.Filled, Color.Red, 1);
+        Tile tile2 = new Tile(Shape.Oval, Shading.Filled, Color.Red, 2);
+        Tile tile3 = new Tile(Shape.Oval, Shading.Filled, Color.Green, 1);
 
-            initialScore = gameState.getFoundSetCount();
-            isValidFalse = gameState.attemptSet(tile1, tile2, tile3);
+        initialScore = gameState.getFoundSetCount();
+        isValidFalse = gameState.attemptSet(tile1, tile2, tile3);
 
-            Assert.assertEquals(initialScore, gameState.getFoundSetCount());
-            Assert.assertFalse(isValidFalse);
-        }
+        Assert.assertEquals(initialScore, gameState.getFoundSetCount());
+        Assert.assertFalse(isValidFalse);
+    }
 
-        public void testValidAttemptingSet() throws Exception {
-            Game gameState2 = new Game();
-            int initialScore2;
-            boolean isValidTrue;
+    public void testValidAttemptingSet() throws Exception {
+        Game gameState2 = new Game();
+        int initialScore2;
+        boolean isValidTrue;
 
-            Tile validTile1 = new Tile(Shape.Oval, Shading.Filled, Color.Red, 1);
-            Tile validTile2 = new Tile(Shape.Oval, Shading.Filled, Color.Red, 2);
-            Tile validTile3 = new Tile(Shape.Oval, Shading.Filled, Color.Red, 3);
+        Tile validTile1 = new Tile(Shape.Oval, Shading.Filled, Color.Red, 1);
+        Tile validTile2 = new Tile(Shape.Oval, Shading.Filled, Color.Red, 2);
+        Tile validTile3 = new Tile(Shape.Oval, Shading.Filled, Color.Red, 3);
 
-            initialScore2 = gameState2.getFoundSetCount();
-            isValidTrue = gameState2.attemptSet(validTile1, validTile2, validTile3);
+        initialScore2 = gameState2.getFoundSetCount();
+        isValidTrue = gameState2.attemptSet(validTile1, validTile2, validTile3);
 
-            Assert.assertEquals(initialScore2 + 1, gameState2.getFoundSetCount());
-            Assert.assertTrue(isValidTrue);
-        }
+        Assert.assertEquals(initialScore2 + 1, gameState2.getFoundSetCount());
+        Assert.assertTrue(isValidTrue);
+    }
 
+    public void testGameTimer() throws Exception {
+        Game game = new Game();
 
-      }
+        // sleep 200 ms
+        Thread.sleep(200);
+
+        long elapsed = game.getElapsedTime();
+        Assert.assertTrue(
+                String.format("Timer should have elapsed about 200ms, but elapsed %dms instead.", elapsed),
+                elapsed >= 200 && elapsed < 400);
+
+        game.pauseTimer();
+        elapsed = game.getElapsedTime();
+
+        Thread.sleep(200);
+        Assert.assertEquals(
+                "Timer should not have elapsed while paused, but it has.",
+                elapsed, game.getElapsedTime());
+
+        game.startTimer();
+
+        Thread.sleep(200);
+        long newElapsed = game.getElapsedTime();
+        Assert.assertTrue(
+                String.format("After resuming, timer should have elapsed about 200ms, but old elapsed time is %dms and new elapsed time is %dms.", elapsed, newElapsed),
+                newElapsed >= elapsed + 200 && newElapsed < elapsed + 400);
+    }
+}
 
