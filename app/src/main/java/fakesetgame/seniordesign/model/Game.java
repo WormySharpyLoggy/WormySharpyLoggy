f07ec9d38ccb95a8d62678e988b6e12ec19ca919
SamiMuhanna <sami.muhanna92@gmail.com>
2014-10-10 14:50:35 -0400

    Game State Class
    Fixed various errors with the game state class

--- app/src/main/java/fakesetgame/seniordesign/model/Game.java
+++ app/src/main/java/fakesetgame/seniordesign/model/Game.java
@@ -8,25 +8,25 @@ import java.util.Set;
  */
 public class Game {
 
-    Board board;
-    int timeElapsed;
-    int score;
-    Set<Set> trackSet = new HashSet<Set>();
-    Set<Tile> tileSet = new HashSet<Tile>();
+    private Board board;
+    private int timeElapsed;
+    private Set<Set> trackSet = new HashSet<Set>();
+
 
     public boolean attemptSet(Tile tile1, Tile tile2, Tile tile3) {
         if (TileSet.isValidSet(tile1, tile2, tile3)) {
+            Set<Tile> tileSet = new HashSet<Tile>();
             tileSet.add(tile1);
             tileSet.add(tile2);
             tileSet.add(tile3);
-            trackSet.add(tileSet);
-            score = score + 1;
-            return true;
+            if (trackSet.add(tileSet)) {
+                return true;
+            }
         }
         return false;
     }
     public int getScore() {
-        return score;
+        return trackSet.size();
     }
 
     public int getTimeElapsed() {
