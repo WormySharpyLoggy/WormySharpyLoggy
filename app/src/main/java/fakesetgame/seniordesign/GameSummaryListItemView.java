9b80cce533805f5ed6840d3368568ccd35fb76a0
Christopher St. John <chrisjstjohn@gmail.com>
2014-10-23 08:35:18 -0400

    Whoops. Won't work without these!

---
+++ app/src/main/java/fakesetgame/seniordesign/GameSummaryListItemView.java
@@ -0,0 +1,11 @@
+package fakesetgame.seniordesign;
+
+import android.content.Context;
+import android.view.View;
+
+public class GameSummaryListItemView extends View {
+
+    public GameSummaryListItemView(Context context) {
+        super(context);
+    }
+}
